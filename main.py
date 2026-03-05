e196e8c3be5188a795c74939ef31dd93bb7f622e
0xhantos <erkankrgl651@gmail.com>
2026-03-05 14:46:32 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:46:26.091Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T11:46:31.108Z
+Commit: Resolve null pointer exception in user service
