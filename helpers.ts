fb0546f687716172401f35cefb0ce8a49f4c8f51
0xhantos <erkankrgl651@gmail.com>
2026-03-05 15:02:57 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:02:52.763Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T12:02:56.531Z
+Commit: Resolve timezone issues in date handling
