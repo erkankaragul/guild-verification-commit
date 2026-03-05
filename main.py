9dfadb4b06d8b12832468f8be6840b455dd89538
0xhantos <erkankrgl651@gmail.com>
2026-03-05 15:05:57 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:05:47.721Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T12:05:56.001Z
+Commit: Add multi-language support
