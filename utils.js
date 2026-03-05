68faf98c716d4cb7d243776315b6685c9124646e
0xhantos <erkankrgl651@gmail.com>
2026-03-05 14:46:04 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:46:00.587Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T11:46:03.683Z
+Commit: Patch security vulnerability in authentication
