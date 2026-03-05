7f876dcbdef892129da32e5ea02df5f80e15ed05
0xhantos <erkankrgl651@gmail.com>
2026-03-05 15:05:36 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:05:27.217Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T12:05:35.520Z
+Commit: Add multi-language support
