d6c929b1afe20915c79e0ba699fe4db464a91691
0xhantos <erkankrgl651@gmail.com>
2026-03-05 14:46:06 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:45:48.850Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T11:46:05.781Z
+Commit: Fix broken links in documentation
