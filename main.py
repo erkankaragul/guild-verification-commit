a6b822dd1c511b53631b10228a8ca0a6a51094d7
0xhantos <erkankrgl651@gmail.com>
2026-03-05 14:39:31 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:39:27.894Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T11:39:30.647Z
+Commit: Resolve null pointer exception in user service
