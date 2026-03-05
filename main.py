5a3b1c099688ad104aecb0dde9ffe4fb23535417
0xhantos <erkankrgl651@gmail.com>
2026-03-05 14:51:26 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:48:08.991Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T11:51:25.153Z
+Commit: Fix broken links in documentation
