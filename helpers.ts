43a5217e2d5d16e3768e061dbc187fee50eff069
0xhantos <erkankrgl651@gmail.com>
2026-03-05 15:02:59 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:02:56.531Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T12:02:57.964Z
+Commit: Patch security vulnerability in authentication
