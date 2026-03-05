50b61c705c1fa14378384739303b5a212e20371e
0xhantos <erkankrgl651@gmail.com>
2026-03-05 15:03:23 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:03:17.101Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T12:03:22.093Z
+Commit: Add multi-language support
