42cb098989092e562836422a307680bbb565163a
0xhantos <erkankrgl651@gmail.com>
2026-03-05 15:05:40 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:05:31.185Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T12:05:39.530Z
+Commit: Fix broken links in documentation
