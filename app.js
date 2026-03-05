f10b39a782e15dee9f4e72e7d065681cbba3227a
0xhantos <erkankrgl651@gmail.com>
2026-03-05 15:05:28 +0300

    Resolve timezone issues in date handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:05:19.252Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T12:05:27.217Z
+Commit: Resolve timezone issues in date handling
