1d171337390eee947e9eb815b0394649046b43c4
0xhantos <erkankrgl651@gmail.com>
2026-03-05 15:04:36 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:04:32.484Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T12:04:35.303Z
+Commit: Fix race condition in async operations
