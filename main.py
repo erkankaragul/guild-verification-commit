7856992f9b5cb7aa519e6492ea1184c714f478ca
0xhantos <erkankrgl651@gmail.com>
2026-03-05 15:03:58 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:03:28.333Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T12:03:57.509Z
+Commit: Fix memory leak in event handlers
