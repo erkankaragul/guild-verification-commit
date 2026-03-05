39717b498d0f45a9395f1dc2089afb8e8a77377c
0xhantos <erkankrgl651@gmail.com>
2026-03-05 14:51:19 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:48:04.855Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T11:51:18.416Z
+Commit: Fix memory leak in event handlers
