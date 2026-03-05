ec76d0c6801b63c62e56ee4effe628518822f325
0xhantos <erkankrgl651@gmail.com>
2026-03-05 14:40:29 +0300

    Patch API endpoint error handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:40:15.696Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T11:40:28.761Z
+Commit: Patch API endpoint error handling
