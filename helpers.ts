772ba1e6aef3da490da7d4919a942459ef95065b
0xhantos <erkankrgl651@gmail.com>
2026-03-05 14:39:52 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:39:48.191Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T11:39:50.863Z
+Commit: Fix broken links in documentation
