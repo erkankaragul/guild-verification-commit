76a48b35a5e95d83ccd5fb45b126889127e8ce9e
0xhantos <erkankrgl651@gmail.com>
2026-03-05 14:55:51 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:51:25.153Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T11:55:50.371Z
+Commit: Patch API endpoint error handling
