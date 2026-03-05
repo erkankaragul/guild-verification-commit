891011121bf68dede3a14612c8206b3b352fc7e1
0xhantos <erkankrgl651@gmail.com>
2026-03-05 15:06:02 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:05:29.818Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T12:06:01.609Z
+Commit: Patch API endpoint error handling
