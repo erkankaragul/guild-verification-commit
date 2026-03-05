592664967d6f6ce1afb1e2c45448063b74fb5258
0xhantos <erkankrgl651@gmail.com>
2026-03-05 15:03:42 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:03:30.717Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T12:03:41.677Z
+Commit: Patch security vulnerability in authentication
