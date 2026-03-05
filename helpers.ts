86d34a4e890264aad00c8617720c442753a77ccd
0xhantos <erkankrgl651@gmail.com>
2026-03-05 14:46:40 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:45:59.307Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T11:46:39.372Z
+Commit: Add multi-language support
