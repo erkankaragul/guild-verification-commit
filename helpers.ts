ecf254ca9d427c6ea3d3c442be20b0505c34947f
0xhantos <erkankrgl651@gmail.com>
2026-03-05 15:05:14 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:04:59.832Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T12:05:13.480Z
+Commit: Patch API endpoint error handling
