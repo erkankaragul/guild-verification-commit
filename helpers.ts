f3cafc31cb90a41cb7563b4c132b26a8735678e0
0xhantos <erkankrgl651@gmail.com>
2026-03-05 14:51:27 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:48:21.887Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T11:51:26.653Z
+Commit: Add export functionality for reports
