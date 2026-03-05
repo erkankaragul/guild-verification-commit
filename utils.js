ffb80ed13f91c2dacd0b9baa5acf11aa317aade6
0xhantos <erkankrgl651@gmail.com>
2026-03-05 14:40:09 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:39:52.167Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T11:40:08.625Z
+Commit: Resolve null pointer exception in user service
