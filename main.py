aa9603ebc5601ee3e91768cc1bad4a60ddb876e3
0xhantos <erkankrgl651@gmail.com>
2026-03-05 14:45:39 +0300

    Fix race condition in async operations

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:40:27.393Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T11:45:38.450Z
+Commit: Fix race condition in async operations
